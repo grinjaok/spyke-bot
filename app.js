d0d724763aecf7450c70b77f75f06ac11d10f95d
NIX\mikhov <daniil.mikhov@gmail.com>
2018-02-22 15:06:36 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const restify = require('restify');
+const builder = require('botbuilder');
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+ console.log('%s listening to %s', server.name, server.url);
+});
+
+const connector = new builder.ChatConnector({
+ appId: process.env.MICROSOFT_APP_ID,
+ appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+const bot = new builder.UniversalBot(connector);
+
+server.post('/api/messages', connector.listen());
+
+bot.dialog('/', (session) => {
+
+session.send("Hello World");
+});
