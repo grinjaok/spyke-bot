81110e354fb008cc81ac8fe59ab150bf60881ef8
NIX\mikhov <daniil.mikhov@gmail.com>
2018-02-24 01:24:32 +0200

    date converted

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const builder = require('botbuilder')
 const eventPlaner = require('./eventPlaner')
 const mongoose = require('mongoose')
 const util = require('util')
+const strftime = require('strftime')
+const strftimeUTC3 = strftime.timezone(120)
 
 const server = restify.createServer()
 const twoMinutes = 2 * 60 * 1000
@@ -22,27 +24,34 @@ const connector = new builder.ChatConnector({
 
 const bot = new builder.UniversalBot(connector)
 
+// const eventInfo = (date, place, description) => {
+//   const convertedDate = strftimeUTC3('%F %T', new Date(date))
+//   return `Event details: <br/>Date/Time: ${convertedDate} <br/>Place: ${place} <br/>Description: ${description}`
+// }
+
 server.post('/api/messages', connector.listen())
 
 setInterval(eventPlaner.comingEvents, 5000)
 
 
-bot.dialog('eventPlanning', [
-  function (session) {
+bot.dialog('eventPlanning', [  
+  (session) => {
       session.send("Welcome to event planning");
       builder.Prompts.time(session, "Please provide a event date and time (e.g.: June 6th at 5pm)");
   },
-  function (session, results) {
+  (session, results) => {
       session.dialogData.eventDate = builder.EntityRecognizer.resolveTime([results.response]);
       builder.Prompts.text(session, "Where is it should be?");
   },
-  function (session, results) {
+  (session, results) => {
       session.dialogData.eventPlace = results.response;
       builder.Prompts.text(session, "Enter event description");
   },
-  function (session, results) {
+  (session, results) => {
       session.dialogData.eventDescription = results.response;
-      session.send(`Event confirmed. Event details: <br/>Date/Time: ${session.dialogData.eventDate} <br/>Place: ${session.dialogData.eventPlace} <br/>Description: ${session.dialogData.eventDescription}`);
+      const convertedDate = strftimeUTC3('%F %T', new Date(session.dialogData.eventDate))
+      const eventInfo = `Event details: <br/>Date/Time: ${convertedDate} <br/>Place: ${session.dialogData.eventPlace} <br/>Description: ${session.dialogData.eventDescription}`
+      session.send(`Event confirmed. ${eventInfo}`);
       eventPlaner.start(session)
       session.endDialog();
   }
@@ -57,7 +66,10 @@ eventPlaner.on('sendNotification', (event) => {
 })
 
 bot.dialog('sendNotification', (session, event) => {
-  // session.send(`Event details: <br/>Date/Time: ${event.eventDate} <br/>Place: ${event.eventPlace} <br/>Description: ${event.eventDescription}`)
-  session.endDialog(`Event details: <br/>Date/Time: ${event.eventDate} <br/>Place: ${event.eventPlace} <br/>Description: ${event.eventDescription}`)
+  const convertedDate = strftimeUTC3('%F %T', new Date(event.eventDate))
+  const eventInfo = `Event details: <br/>Date/Time: ${convertedDate} <br/>Place: ${event.eventPlace} <br/>Description: ${event.eventDescription}`  
+  session.endDialog(eventInfo)
 });
+
+
 
